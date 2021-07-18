8fac309b19ca494c1ba34ba2198624eae561a87a
mohamedhamdy123890456@gmail.com <mohamedhamdy123890456@gmail.com>
2021-07-18 20:45:29 +0200

    /

--- remve_backspace/__manifest__.py
+++ remve_backspace/__manifest__.py
@@ -8,22 +8,20 @@
     'description': """
        
     """,
+    'price': 49.99,
 
     'author': "Mohamed Hamdy",
     'website': "mohamedhamdy123890456@gmail.com",
 
-   
     'category': 'POS',
     'version': '0.1',
 
-
-    'depends': ['base','point_of_sale'],
-
+    'depends': ['base', 'point_of_sale'],
 
     'qweb': [
         'static/src/xml/Chrome.xml',
     ],
-    
+
     'images': [
         'static/description/cover.png',
         'static/description/icon.png',
