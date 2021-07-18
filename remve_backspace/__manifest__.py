3dffccbedb97b8376de8d9ddba4050540c8fd4ee
mohamedhamdy123890456@gmail.com <mohamedhamdy123890456@gmail.com>
2021-07-18 19:45:08 +0200

    /

---
+++ remve_backspace/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "remve_backspace",
+
+    'summary': """
+        """,
+
+    'description': """
+       
+    """,
+
+    'author': "Mohamed Hamdy",
+    'website': "mohamedhamdy123890456@gmail.com",
+
+   
+    'category': 'POS',
+    'version': '0.1',
+
+
+    'depends': ['base','point_of_sale'],
+
+
+    'qweb': [
+        'static/src/xml/Chrome.xml',
+    ],
+
+}
